d2130fd264d16fa94c49afd20f0e4be070c5d693
AnissaBS <aniissa.bensaid@gmail.com>
2023-10-16 16:43:40 +0200

    Travaux php sur les variables

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
+    <a href="opSurVar.php">Opération sur les variables.</a>
 
 <?php
 // J'affiche le prénom
@@ -34,14 +35,61 @@ echo "<br> j'envoie des lettres à \"ma dulcinée\"";
 
 /* Les variables
 Une variable est un conteneur d'une donnée unique, c'est à dire une boîte à chaussures qui ne contient qu'une seule chaussure.
-Une variable contient une information temporaire qui peut être numérique, de type alphabétique (chaîne de caractères), de type date, de type objet ou d'autres types.
-Le nom d'une variable doit commencer le caractère $ suivi d'une lettre ou d'un signe _ (barre du huit) et ne doit jamais commencer par un chiffre.Les noms des variables sont sensibles à la casse (miniscule/majuscule).
+Une variable contient une information temporaire qui peut être numérique, de type alphabétique (chaîne de caractères), de type date, de type objet ou d'autres types. */
+
+echo "<br>";
+$largeur = 5;
+echo $largeur;
+
+/* Le nom d'une variable doit commencer le caractère $ suivi d'une lettre ou d'un signe _ (barre du huit) et ne doit jamais commencer par un chiffre.Les noms des variables sont sensibles à la casse (miniscule/majuscule).
 Le signe = sert donc à affecter une valeur à la variable, il s'agit donc d'un opérateur d'affectin et non le signe égal que l'on utilise pour calculer. */
 
 $prenom = "téo";
 $nom = "Durant";
 $prenom_nom = $prenom."".$nom;
 echo '<br>'.$prenom_nom;
+echo "<br>";
+
+/**
+ * Fonctions qui s'appliquent aux variables
+ * 
+ * strlen(variable) : renvoie la longueur d'une chaîne de caractères.
+ * substr(variable, début, longueur) : renvoie une partie de la chaîne de caractères.
+ * trim(variable) : supprime les espaces (ou d'autres caractères) en début et en fin de chaîne.
+ * number_format(variable "point ou point virgule"; "caractères pour les milliers").
+ * round(variable, nombre de décimale) : arrondit le nombre.
+ */
+
+ $nom2 = "MARIE ROSE";
+ echo strlen($nom2); // Affiche le nombre de caractères de la variable "$nom2" : ici, 10 à cause des espaces
+ echo "<br>";
+ var_dump($nom2); // Affiche string (10) "MARIE ROSE"
+ echo "<br>";
+ echo trim($nom2); // Affiche MARIE ROSE sans espaces
+ echo "<br>";
+ echo substr($nom2, 3, 5);
+ echo "<br>";
+ echo $prix = 2050900.7854;
+ echo "<br>";
+ echo number_format($prix, 2, ", ", ","); // Affiche 2 050 900.79 en plaçant 2 décimales après un ppoint et un espace comme séparateur de milliers.
+ echo "<br>";
+ echo round($prix, 1); // Arrondit à une décimale près
+ echo "<br>";
+
+ // AFFECTATION DE VARIABLES PAR LA VALEUR OU PAR ALIAS
+
+ /**
+  * Losqu'on affecte une donnée dans une variable, on dit qu'on affecte la variable par valeur. Toutefois, on pourrait aussi créer un alias à une autre variable. On dit parfois qu'on affecte la variable par référence.
+  * Pour créer un alias vers une autre variable, on utilise le signe & (et commercial ou perpluette).
+  */
+
+  $v1 = "Jacques Chirac <br>";
+  $alias = &$v1; // Créer un alias vers $v1
+  $alias = "Mon nom est $alias"; // Affectation par référence : affecter une valeur à l'aide d'une autre variable
+  echo $alias;
+
+  $prenom2 = "Alain";
+  echo "Salut les gars mon nom est : ".$prenom2." Dupont";
 
 ?>
 </body>
---
+++ opSurVar.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        $hab = 15230;
+        echo $hab;
+
+        $hab = 102;
+        echo $hab;
+        
+        echo "<br>";
+
+        $ville = "Orléans";
+        $dep = "Centre";
+        $titre = $ville.", ".$dep;
+        echo $titre;
+
+        echo "<br>";
+
+        $nn = "Salut";
+        $mm = "Bob";
+        $pp = "Copaiiiiing";
+
+        $affiche = $nn." ".$mm.", tu veux être mon ".$pp."?";
+        echo $affiche;
+        echo "<br>";
+
+        $a = 1;
+        $b = 3;
+        $a = $a + 2;
+        echo "Premier : " .$a;
+        echo "<br>";
+
+        $a = $a * $b;
+        echo "Deuxième : " .$a;
+        echo "<br>";
+
+        $a = $a -1;
+        echo "Troisième : " .$a;
+        echo "<br>";
+
+        $a = $a /4;
+        echo "Quatrième : " .$a;
+        echo "<br>";
+
+
+    ?>
+
+</body>
+</html>
