280048754def13b2b13922a317ae9a08d1927665
AnissaBS <139979527+AnissaBS@users.noreply.github.com>
2023-10-24 16:34:14 +0200

    Add files via upload

--- boucles.php
+++ boucles.php
@@ -22,7 +22,7 @@
     /*
         Elle exécute un bloc de lignes de code tant que la condition est vraie.
     */
-        echo ". Boucle WHILE";
+        echo "<b>. Boucle WHILE</b>";
         echo "<br><br>";
         $a = 1;
         // Tant que la variable est inférieure ou égale à 10
@@ -40,7 +40,7 @@
         En d'autres termes, vous êtes sûr que le 1er passage sera effectué dans la boucle même si la condition s'avère fausse.
     */
 
-        echo ". Boucle DO WHILE";
+        echo "<b>. Boucle DO WHILE</b>";
         echo "<br><br>";
 
     // je définis  l'environnement en français
@@ -65,7 +65,7 @@
     
     // Notions d'incrémentation et de décrémentation
 
-        echo ". Notion d'incrémentation et de décrémentation";
+        echo "<b>. Notion d'incrémentation et de décrémentation</b>";
 
         $x = 20;
         echo "<br><br>";
@@ -92,14 +92,14 @@
         - une incrémentation, qui s'effectue à la  fin de hcaque passage dans la boucle
     */
         echo"<br><br><br>";
-        echo ". BOUCLE FOR";
+        echo "<b>. BOUCLE FOR</b>";
         echo"<br><br>";
 
         for($p=0; $p<=5; $p++){
             echo "Je dis bonjour avec la boucle for : ".$p."<br>";
         }
 
-        echo "Les niveau de titres en html";
+        echo "Les niveaux de titres en html";
         for($i=1; $i<7; $i++) {
             echo "<h$i> $i : Titre de niveau $i</h$i>";
         }
@@ -108,34 +108,63 @@
         for($i=1, $j=9; $i<10, $j>0; $i++, $j--) {
             echo "<span style =\"border-style:double;border-width:3;\"> $i + $j=10</span>";
         }
-        echo "<br><br>";
+        echo "<br><br><br>";
 
-        echo "<h2>Révisez votre table de multiplication</h2>";
-        // Début du tableau
-        echo "<table border = \"2" style=\"background-color:yellow\">
-            <th>&nbsp; X &nbsp;</th>";
+        echo "<h3>. Révisez votre table de multiplication</h3><br>";
 
-        // Créer une première ligne
+    // Début du tableau
+        echo "<table border =\"2\" style=\"background-color:rgb(100, 100, 180)\"><tr><th>&nbsp; X &nbsp;</th>";
+
+    // Créer une première ligne
         for($i=1; $i<10; $i++) {
             echo "<th> &nbsp; $i&nbsp; </th>";
-            // Fin de la boucle
-            for($i=1; $1<10; $i++){
-                echo "<th> &nbsp; $i&nbsp; </th>";
-            }
-            //Remplissage de la table
+        }
+
+        echo "</tr>";
+
+    // Fin de la boucle
+    // Création du corps de la table :
+    // Boucles de création du contenu de la table
+        for($i=1; $i<10; $i++){
+            echo "<tr><th> &nbsp; $i&nbsp; </th>";
+
+        //Remplissage de la table
             for ($j=1; $j<10; $j++){
-                echo "<td style=\background-color:red"
+                echo "<td style=\"background-color:rgb(147, 147, 222);color:white\">&nbsp;".$i*$j."&nbsp; &nbsp; </td>";
             }
         }
 
+        echo "</tr>";
+
+        echo "</table><br><br>";
+
+    // Autre tableau
+        echo "<h5>Autre mise en forme de tableau</h5>";
+        
+        echo "<table border=2 width=\"50%\" style=\"color:white;\">"; // Création de la balise table avec bordure, largeur 50% et couleur du texte blanc
+        echo "<tr><td style=\"background-color: rgb(0, 15, 225);\">X</td>"; // Début de la première ligne et déclaration de la première cellule qui ne s'incrémentera pas X
+        for($i=1;$i<=10;$i++){ // Déclaration de la boucle de la première ligne
+            echo "<td style=\"background-color: rgb(" . ($i * 10) . ", 15, 225);\">" . $i . "</td>"; //Écriture des cellules de la première ligne avec un bckground qui change de couleur avec la boucle
+        }
+        echo "</tr>"; //Fin de la première ligne
+        for($i=1;$i<=10;$i++){ //déclaration de la boucle pour le tableau
+            echo "<tr><td style=\"background-color: rgb(" . ($i * 10) . ", 15, 225);\">".$i."</td>"; //première cellule la ligne
+            for($j=1;$j<=10;$j++){ //boucle qui remplit la ligne à partir de la 2ème cellule
+                echo "<td style=\"background-color: rgb(" . ($i * 10) . "," . ($j * 10) . ", 225);\">".($i*$j)."</td>";
+            }
+            echo "</tr>"; //fin de ligne
+        }
+
+        echo "</table>"; //Fermeture de la table
+
 
     //La boucle FOREACH
-    /*
-    
-    */
+        /*
+        
+        */
 
         echo"<br><br><br>";
-        echo ". BOUCLE FOREACH";
+        echo "<b>. BOUCLE FOREACH</b>";
         echo"<br>";
 
         $T["janvier "] = " école";
@@ -144,7 +173,6 @@
         foreach($T as $mois => $periode){
             echo $mois."===>".$periode."<br>";
         }
-
 
     ?>
 
--- tableau.php
+++ tableau.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styleGrid.css">
     <title>Document</title>
+    <style>
+        table,td,th,tr{
+            border: 2px solid black;
+        }
+    </style>
 </head>
 <body>
     
@@ -19,13 +24,13 @@
 <?php
     $tb = ['Paris', 'Londres', 'Rome', 'Berlin'];
     // echo $tb[1];
-echo "sans count <br>";
+echo ". Sans count <br>";
 for($i=0;$i<3;$i++){
     echo $tb[$i], "<br>";
 }
 
 echo "<br>";
-echo "avec count<br>";
+echo ". Avec count<br>";
 for($i=0; $i<count($tb);$i++){
     echo $tb[$i], "<br>";
 }
@@ -75,7 +80,77 @@ echo "<br><br>";
         echo "Le client de login <b>$cle</b> a le code <b>$val</b> <br>";
     }
 
-?>
+    echo "<br><br>";
+
+    // Création du tableau
+    for($i=0; $i<=10; $i++){
+        $tab3[$i] = pow(2, $i);
+    }
+
+    // La lecture des indices et des valeurs
+    foreach($tab3 as $ind=>$val){
+        echo "<br> 2 puissances $ind vaut $val";
+    }
+
+    echo "<br><br> Le dernier indice :".$ind," , la dernière valeur :", $val, "<br><br>";
+
+    $Teb = array(
+        array ("<b>LIBRAIRIE</b>", "<b>TITRES</b>", "<b>REMISE</b>"),
+        array ("FNAC", "GOOGLE SHEETS", "40%"),
+        array ("FNAC", "EXCEL", "40%"),
+        array ("EXPRESS", "GOOGLE DOCS", "38%"),
+        array ("EXPRESS", "GOOGLE SLIDES", "40%")
+    );
+    echo "<table border=1 width=\"50%\">";
+
+    // La lecture de chaque ligne
+    foreach($Teb as list($librairie, $titre,$remise)){
+        // J'affiche les données dans des cases d'un tableau
+        echo "<tr><td>".$librairie."</td><td>".$titre."</td>"."<td>".$remise."</td>"."</tr><br>";
+    }
+    echo "</table>";
+
+    echo "<br><br>";
+
+    $tablo[1]= "Pomme";
+    $tablo[2]= "Poire";
+    $tablo[3]= "Orange";
+    $tablo[4]= "Cerise";
+    $tablo[5]= "Mangue";
+
+    // Je relie les éléments en les affichant dans le browser
+    echo "<ol>"; // Je prépare une liste numérotée
+    for($i=1;$i<=5;$i++){
+        echo "<li>".$tablo[$i]."</li>"; //J'affiche chaque élément du tableau
+    }
+    echo "</ol>";
+
+    echo "<br><br>";
+
+    $tablo1[]="Stylo";
+    $tablo1[]="Crayon";
+    $tablo1[]="Gomme";
+    $tablo1[]="Règle";
+
+    // Je relie les éléments en commençant à 0
+    for($i=0; $i<count($tablo1); $i++){
+        echo $tablo1[$i]."<br>";
+    }
+
+    echo "<br><br>";
+
+    $tablo2= ["Voiture", "Vélo", "Moto", "Camion", "Bateau"];
+    echo "<br>";
+    print_r($tablo2);
+
+    /* En résumé :
+        Les tableaux sont des structures de variables complexes à une ou plusieurs dimensions.
+        Un indice numérique ou texte permet de récupérer la valeur d'une case d'un tableau. Duns un indice numérique, on utilise une boucle for ou while pour lire les éléments d'un tableau.
+        Dans le cas d'un indice de type texte, il faut employer la boucle foreach.
+        Les tableaux servent aussi à organiser des matrices d'informations que l'on récupère si besoin en les parcourant à l'aide d'une ou plusieurs boucles.
+    */
+
+    ?>
 </div>
 
 <?php
