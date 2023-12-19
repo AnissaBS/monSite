2dd881b4cca2fbf67c485b6c7655e030b41b7d1a
AnissaBS <139979527+AnissaBS@users.noreply.github.com>
2023-12-19 16:30:31 +0100

    Add files via upload

--- boucles.php
+++ boucles.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styleGrid.css">
-    <title>Strucutres de contrôle</title>
+    <title>Structures de contrôle</title>
 </head>
 <body>
 
